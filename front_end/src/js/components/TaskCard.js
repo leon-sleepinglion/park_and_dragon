f4e1d3b8341daa7bb1c492fa490caad849780253
kamwoh <kamwoh@neruti.com>
2018-04-28 18:02:01 +0800

    Done task card

---
+++ front_end/src/js/components/TaskCard.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import { Card, Spin } from 'antd'
+
+const valueStyle = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  wordBreak: 'break-all',
+  whiteSpace: 'nowrap',
+  color: 'fade(#000, 85%)',
+  marginTop: '4px',
+  marginBottom: 0,
+  fontSize: '30px',
+  lineHeight: '38px',
+  height: '38px'
+}
+
+const renderValue = value => {
+  let totalDom
+  switch (typeof value) {
+    case 'undefined':
+      totalDom = null
+      break
+    case 'function':
+      totalDom = <div style={ valueStyle }>{ value() }</div>
+      break
+    default:
+      totalDom = <div style={ valueStyle }>{ value }</div>
+  }
+  return totalDom
+}
+
+
+const chartCardStyle = {
+  position: 'relative'
+}
+
+const chartCardTopStyle = {
+  position: 'relative',
+  overflow: 'hidden',
+  width: '100%',
+  marginBottom: '12px'
+}
+
+const avatarStyle = {
+  position: 'relative',
+  top: '4px',
+  float: 'left',
+  marginRight: '20px',
+  img: {
+    borderRadius: '100%'
+  }
+}
+
+const metaStyle = {
+  color: 'fade(#000, 45%)',
+  fontSize: '14px',
+  lineHeight: '22px',
+  height: '22px'
+}
+
+const pointStyle = {
+  position: 'absolute',
+  top: 0,
+  right: 0
+}
+
+const locationStyle = {}
+
+const contentFixedStyle = {
+  position: 'absolute',
+  left: 0,
+  bottom: 0,
+  width: '100%'
+}
+
+const footerStyle = {
+  borderTop: '1px solid rgba(0,0,0,0.1)',
+  paddingTop: '9px',
+  marginTop: '8px',
+  '& > *': {
+    position: 'relative'
+  }
+}
+
+const itemStyle = {
+  display: 'inline-block',
+  fontSize: '14px',
+  lineHeight: '22px'
+}
+
+const itemValueStyle = {}
+
+const TaskCard = ({
+                    contentHeight,
+                    missionLocation,
+                    avatar,
+                    missionPoint,
+                    missionName,
+                    footer,
+                    children,
+                    ...rest
+                  }) => {
+  const content = (
+    <div style={ chartCardStyle }>
+      <div style={ chartCardTopStyle }>
+        <div style={ avatarStyle }>{ avatar }</div>
+        <div style={ { 'float': 'left' } }>
+          <div style={ metaStyle }>
+            <span style={ locationStyle }>{ missionLocation }</span>
+            <span style={ pointStyle }>{ missionPoint }</span>
+          </div>
+          { renderValue(missionName) }
+        </div>
+      </div>
+      { children && (
+        <div style={ {
+          height: contentHeight || 'auto',
+          marginBottom: '12px',
+          position: 'relative',
+          width: '100%'
+        } }>
+          <div style={ contentHeight && contentFixedStyle }>{ children }</div>
+        </div>
+      ) }
+      { footer && (
+        <div style={ footerStyle }>
+          { footer }
+        </div>
+      ) }
+    </div>
+  )
+
+  return (
+    <Card bodyStyle={ { padding: '20px 24px 8px 24px' } } { ...rest }>
+      <div style={ itemStyle }>
+        <span style={ itemValueStyle }>{ content }</span>
+      </div>
+    </Card>
+  )
+}
+
+export default TaskCard
--- front_end/src/js/pages/Tasks.js
+++ front_end/src/js/pages/Tasks.js
@@ -1,7 +1,91 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Layout, Row, } from 'antd'
+import { Field, TaskCard } from '../components'
 
-export default class Tasks extends React.Component {
+const { Content } = Layout
+
+const topColResponsiveProps = {
+  xs: 24,
+  sm: 12,
+  md: 12,
+  lg: 12,
+  xl: 6,
+  style: { marginBottom: 24 },
+};
+
+export default class Tasks extends Component {
+  state = {}
+
   render() {
-    return <p>Tasks</p>
+    const linkCard = {
+      content: {
+        missionId: '1234',
+        missionName: 'Mission A',
+        missionDescription: 'This is to buy something from store A',
+        missionLocation: 'Location A',
+        missionPoint: '100',
+        missionStatus: 0 // 0 = not yet, 1 = doing, 2 = done
+      }
+    }
+    const cols = [
+      linkCard,
+      linkCard,
+      linkCard,
+      linkCard
+    ]
+    const rows = [
+      cols,
+      cols,
+      cols,
+      cols
+    ]
+
+    const fieldValue = {
+      0: 'Failed',
+      1: 'On Going',
+      2: 'Done'
+    }
+
+
+    const fieldValueStyle = {
+      0: '#e74c3c',
+      1: '#3498db',
+      2: '#2ecc71'
+    }
+
+
+    return (
+      <Layout>
+        <Content style={ { margin: '24px 24px 0', height: '100%' } }>
+          <Fragment>
+            { rows.map((col, index) => {
+              return (
+                <Row key={ `row${index}` } gutter={ 24 }>
+                  { col.map((card, cardIndex) => {
+                    return (
+                      <Col key={ `row${index}col${cardIndex}` } { ...topColResponsiveProps }>
+                        <TaskCard
+                          bordered={ false }
+                          missionLocation={ card.content.missionLocation }
+                          missionPoint={ card.content.missionPoint }
+                          missionName={ card.content.missionName }
+                          footer={ <Field label="Status:"
+                                          labelStyle={ { marginRight: 10 } }
+                                          value={ fieldValue[card.content.missionStatus] }
+                                          valueStyle={ { color: fieldValueStyle[card.content.missionStatus] } }/> }
+                          key={ `row${index}col${cardIndex}task${cardIndex}` }
+                        >
+                          <Field value={ card.content.missionDescription } style={ { overflow: 'display' } }/>
+                        </TaskCard>
+                      </Col>
+                    )
+                  }) }
+                </Row>
+              )
+            }) }
+          </Fragment>
+        </Content>
+      </Layout>
+    );
   }
 }
