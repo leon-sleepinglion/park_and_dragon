5e6e1f3dcb669e69c15025b4c5c215a04fd8fee3
kamwoh <kamwoh@neruti.com>
2018-04-28 18:55:41 +0800

    Done task card

--- front_end/src/js/pages/Tasks.js
+++ front_end/src/js/pages/Tasks.js
@@ -11,13 +11,24 @@ const topColResponsiveProps = {
   lg: 12,
   xl: 6,
   style: { marginBottom: 24 },
-};
+}
+
+const fieldValue = {
+  0: 'Failed',
+  1: 'On Going',
+  2: 'Done'
+}
+
+const fieldValueStyle = {
+  0: '#e74c3c',
+  1: '#3498db',
+  2: '#2ecc71'
+}
 
 export default class Tasks extends Component {
-  state = {}
 
   render() {
-    const linkCard = {
+    const missionCard = {
       content: {
         missionId: '1234',
         missionName: 'Mission A',
@@ -27,65 +38,46 @@ export default class Tasks extends Component {
         missionStatus: 0 // 0 = not yet, 1 = doing, 2 = done
       }
     }
-    const cols = [
-      linkCard,
-      linkCard,
-      linkCard,
-      linkCard
+    const missions = [
+      missionCard,
+      missionCard,
+      missionCard,
+      missionCard,
+      missionCard,
+      missionCard,
+      missionCard,
+      missionCard
     ]
-    const rows = [
-      cols,
-      cols,
-      cols,
-      cols
-    ]
-
-    const fieldValue = {
-      0: 'Failed',
-      1: 'On Going',
-      2: 'Done'
-    }
-
-
-    const fieldValueStyle = {
-      0: '#e74c3c',
-      1: '#3498db',
-      2: '#2ecc71'
-    }
 
 
     return (
       <Layout>
         <Content style={ { margin: '24px 24px 0', height: '100%' } }>
           <Fragment>
-            { rows.map((col, index) => {
-              return (
-                <Row key={ `row${index}` } gutter={ 24 }>
-                  { col.map((card, cardIndex) => {
-                    return (
-                      <Col key={ `row${index}col${cardIndex}` } { ...topColResponsiveProps }>
-                        <TaskCard
-                          bordered={ false }
-                          missionLocation={ card.content.missionLocation }
-                          missionPoint={ card.content.missionPoint }
-                          missionName={ card.content.missionName }
-                          footer={ <Field label="Status:"
-                                          labelStyle={ { marginRight: 10 } }
-                                          value={ fieldValue[card.content.missionStatus] }
-                                          valueStyle={ { color: fieldValueStyle[card.content.missionStatus] } }/> }
-                          key={ `row${index}col${cardIndex}task${cardIndex}` }
-                        >
-                          <Field value={ card.content.missionDescription } style={ { overflow: 'display' } }/>
-                        </TaskCard>
-                      </Col>
-                    )
-                  }) }
-                </Row>
-              )
-            }) }
+            <Row gutter={ 24 }>
+              { missions.map((card, cardIndex) => {
+                return (
+                  <Col key={ `col${cardIndex}` } { ...topColResponsiveProps }>
+                    <TaskCard
+                      bordered={ false }
+                      missionLocation={ card.content.missionLocation }
+                      missionPoint={ card.content.missionPoint }
+                      missionName={ card.content.missionName }
+                      footer={ <Field label="Status:"
+                                      labelStyle={ { marginRight: 10 } }
+                                      value={ fieldValue[card.content.missionStatus] }
+                                      valueStyle={ { color: fieldValueStyle[card.content.missionStatus] } }/> }
+                      key={ `col${cardIndex}task${cardIndex}` }
+                    >
+                      <Field value={ card.content.missionDescription } style={ { overflow: 'display' } }/>
+                    </TaskCard>
+                  </Col>
+                )
+              }) }
+            </Row>
           </Fragment>
         </Content>
       </Layout>
-    );
+    )
   }
 }
